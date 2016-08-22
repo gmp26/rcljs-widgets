69724c6b9d3f8e62fa4f3caec4ece7a516d6f6e7
gmp26 <gmp26@cam.ac.uk>
2016-08-22 21:26:00 +0100

    top level devcards list working

--- project.clj
+++ project.clj
@@ -2,17 +2,17 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.5.3"
-  
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
                  [devcards "0.2.1-7"]
                  [rum "0.10.4"]
                  [figwheel-sidecar "0.5.4-6"]
                  [pubsub "0.1.0-SNAPSHOT"]
-                  ]
+                 ]
 
   :plugins [
             ;[lein-figwheel "0.5.3-2"]
@@ -23,33 +23,34 @@
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                     "target"]
-  
+
   :source-paths ["src" ".lein-git-deps/pubsub/src"]
 
   :cljsbuild {
-              :builds [{:id "devcards"
+              :builds [{:id           "devcards"
                         :source-paths ["src"]
-                        :figwheel { :devcards true } ;; <- note this
-                        :compiler { :main       "rcljs-widgets.devcards" ;; <- and this!
-                                    :asset-path "js/compiled/devcards_out"
-                                    :output-to  "resources/public/js/compiled/rcljs_widgets_devcards.js"
-                                    :output-dir "resources/public/js/compiled/devcards_out"
-                                    :source-map-timestamp true
-                                   }}
-                       {:id "dev"
+                        :figwheel     {:devcards true}      ;; <- note this
+                        :compiler     {;;:main                 "rcljs-widgets.devcards" ;; <- and this!
+                                       :main                 "example.start-ui" ;; <- and this!
+                                       :asset-path           "js/compiled/devcards_out"
+                                       :output-to            "resources/public/js/compiled/rcljs_widgets_devcards.js"
+                                       :output-dir           "resources/public/js/compiled/devcards_out"
+                                       :source-map-timestamp true
+                                       }}
+                       {:id           "dev"
                         :source-paths ["src"]
-                        :figwheel true
-                        :compiler {:main       "rcljs-widgets.core"
-                                   :asset-path "js/compiled/out"
-                                   :output-to  "resources/public/js/compiled/rcljs_widgets.js"
-                                   :output-dir "resources/public/js/compiled/out"
-                                   :source-map-timestamp true }}
-                       {:id "prod"
+                        :figwheel     true
+                        :compiler     {:main                 "rcljs-widgets.core"
+                                       :asset-path           "js/compiled/out"
+                                       :output-to            "resources/public/js/compiled/rcljs_widgets.js"
+                                       :output-dir           "resources/public/js/compiled/out"
+                                       :source-map-timestamp true}}
+                       {:id           "prod"
                         :source-paths ["src"]
-                        :compiler {:main       "rcljs-widgets.core"
-                                   :asset-path "js/compiled/out"
-                                   :output-to  "resources/public/js/compiled/rcljs_widgets.js"
-                                   :externs ["externs/exported.js"]
-                                   :optimizations :whitespace}}]}
+                        :compiler     {:main          "rcljs-widgets.core"
+                                       :asset-path    "js/compiled/out"
+                                       :output-to     "resources/public/js/compiled/rcljs_widgets.js"
+                                       :externs       ["externs/exported.js"]
+                                       :optimizations :whitespace}}]}
 
-  :figwheel { :css-dirs ["resources/public/css"] })
+  :figwheel {:css-dirs ["resources/public/css"]})
---
+++ src/example/start_ui.cljs
@@ -0,0 +1,12 @@
+(ns example.start-ui
+  (:require
+    [devcards.core]
+    [rcljs-widgets.devcards]
+    [rcljs-widgets.reactjs-devcards]))
+
+;; The main function here is actually used in a documentation
+;; generator that I'm experimenting with. This is not needed
+;; with a standard Devcards setup!!
+
+(defn ^:export main []
+  (devcards.core/start-devcard-ui!))
