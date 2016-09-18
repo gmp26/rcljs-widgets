68ebca7b60fc097d5f0af65bc1740f7f913afe2e
gmp26 <gmp26@cam.ac.uk>
2016-09-18 22:28:43 +0100

    tidy up

--- src/svg/scales.cljs
+++ src/svg/scales.cljs
@@ -30,9 +30,6 @@
       (+ (* (Math.floor (/ stop step)) step) (/ step 2))
       step)))
 
-
-
-
 (defn- scale-ticks [a-scale tick-count]
   (apply preferred-ticks (conj (:in a-scale) tick-count)))
 
@@ -63,23 +60,6 @@
 
 (defn nice-identity [in tick-count]
   (->Identity (linear-nice in tick-count) tick-count))
-
-;scale.nice = function (count)
-;{
-; var d = domain (),
-;     i = d.length - 1,
-; n   = count == null ? 10 : count,
-;     start = d [0],
-;     stop = d [i],
-;     step = tickStep (start, stop, n)                       ;
-;
-;     if (step) {
-;                step   = tickStep (Math.floor (start / step) * step, Math.ceil (stop / step) * step, n) ;
-;                d      [0] = Math.floor (start / step) * step ;
-;                       d [i] = Math.ceil (stop / step) * step ;
-;                domain (d)                                  ;
-;                }
-
 
 (defrecord Linear [in out tick-count]
   IScale
